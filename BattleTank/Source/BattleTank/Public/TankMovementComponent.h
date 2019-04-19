7b0b49bc72cb8d15f889f9e52aa2e57838cc047d
Noah Surprenant <noah.surprenant@gmail.com>
2019-04-18 20:09:45 -0400

    Fly-by-Wire Control System

---
+++ BattleTank/Source/BattleTank/Public/TankMovementComponent.h
@@ -0,0 +1,17 @@
+// Noah Surprenant - UnrealCourse.com
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/NavMovementComponent.h"
+#include "TankMovementComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_API UTankMovementComponent : public UNavMovementComponent
+{
+	GENERATED_BODY()
+	
+};
